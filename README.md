135f3e3f39bd3c0562249bca195fc81f45936919
Mille Kåge <89786843+Wolframe107@users.noreply.github.com>
2024-03-27 18:58:49 +0100

    Tweaks and better readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# gramfactory
-A python script that generates stereograms.
+## gramfactory
+
+Gramfactory is a Python script designed to generate autostereograms, commonly known as stereograms.
+
+### How to Use
+
+Configuration settings are adjusted within the `config.json` file. Simply select your desired background and mask, then execute `main.py`. Choose from the provided images in "Backgrounds" or "Masks," or craft your own. It's crucial to note that the mask must be in PNG format and utilize transparency for optimal results.
+
+### Understanding Configuration Variables
+
+- **num_of_columns**: Determines how many times the background slice repeats. Recommended values: 10-15
+  
+- **num_of_slices**: Specifies the number of slices over which the mask stretches, influencing its scale. Recommended values: 2-5
+  
+- **offset**: Reflects the depth of the mask, contributing to the 3D effect. However, excessively large values may compromise the illusion. Recommended values: 5-25
+
+- **background_location**: Points to the desired background image slice for the stereogram.
+
+- **mask_location**: Indicates the location of the mask image to be used, with PNG format being the only acceptable option.
--- main.py
+++ main.py
@@ -1,67 +1,82 @@
 from PIL import Image
+import json
 
-# Settings
-col_num = 15  # Num of repeating columns
-offset = 25  # Z-offset for slices
-num_of_slices = 9  # Over how many columns does the mask sit
 
-# Set up background
-pattern = Image.open("Images/test_bak2.jpg")
-width = pattern.width
-height = pattern.height
+def load_config(filename):
+    with open(filename, "r") as f:
+        return json.load(f)
 
-# Set up mask and scale according to number of slices
-mask = Image.open("Images/star.png")
-new_height = int((num_of_slices * width * mask.height) / mask.width)
-mask = mask.resize(((num_of_slices) * width, new_height))
+def main():
+    # Settings
+    config = load_config("config.json")
+    num_of_columns = config.get("num_of_columns")
+    num_of_slices = config.get("num_of_slices")
+    offset = config.get("offset")
+    background_location = config.get("background_location")
+    mask_location = config.get("mask_location")
 
-if mask.height > height:
-    print("num_of_slices is too high")
-    exit()
+    # Set up background
+    pattern = Image.open(background_location)
+    width = pattern.width
+    height = pattern.height
 
-def pasteMask(slice):
-    # Create transparent mask in shape of background
-    mask_size = (
-        (mask.width * slice) / num_of_slices,
-        0,
-        (mask.width * (slice + 1)) / num_of_slices,
-        mask.height,
-    )
+    # Set up mask and scale according to number of slices
+    mask = Image.open(mask_location)
+    new_height = int((num_of_slices * width * mask.height) / mask.width)
+    mask = mask.resize(((num_of_slices) * width, new_height))
 
-    mask_cropped = mask.crop(mask_size)
-    result_mask = Image.new("RGBA", (width, mask.height), (255, 255, 255, 0))
-    result_mask.paste(mask_cropped, (0, 0))
+    if mask.height > height:
+        print("num_of_slices is too high")
+        exit()
 
-    # Crop background to fit mask
-    left_slice = int(col_num / 2) - 1 + slice
-    temp_background = result.crop(
-        (
-            left_slice * width - (offset * slice),
-            int(height / 2 - result_mask.height / 2),
-            (left_slice + 1) * width - (offset * slice),
-            int(height / 2 + result_mask.height / 2),
+    def pasteMask(slice):
+        # Create transparent mask in shape of background slice
+        mask_size = (
+            (mask.width * slice) / num_of_slices,
+            0,
+            (mask.width * (slice + 1)) / num_of_slices,
+            mask.height,
         )
-    )
 
-    # Paste unto background
-    for i in range(col_num - left_slice):
-        result.paste(
-            temp_background,
+        mask_cropped = mask.crop(mask_size)
+        result_mask = Image.new("RGBA", (width, mask.height), (255, 255, 255, 0))
+        result_mask.paste(mask_cropped, (0, 0))
+
+        # Keep track of left most slice + a little voodoo to make mask centered
+        if(num_of_slices % 2 == 0):
+            left_slice = int(num_of_columns / 2) + slice - int(num_of_slices/2) + 1
+        else:
+            left_slice = int(num_of_columns / 2) + slice - int(num_of_slices/2)
+
+        # Crop background to fit the mask
+        temp_background = result.crop(
             (
-                width * (i - 1 + left_slice + 1) - offset * (slice + 1),
-                int(height / 2 - temp_background.height / 2),
-            ),
-            mask=result_mask,
+                left_slice * width - (offset * slice),
+                int(height / 2 - result_mask.height / 2),
+                (left_slice + 1) * width - (offset * slice),
+                int(height / 2 + result_mask.height / 2),
+            )
         )
 
+        # Paste masked background back to the result background
+        for i in range(num_of_columns - left_slice):
+            result.paste(
+                temp_background,
+                (
+                    width * (i - 1 + left_slice + 1) - offset * (slice + 1),
+                    int(height / 2 - temp_background.height / 2),
+                ),
+                mask=result_mask,
+            )
 
-result = Image.new(mode="RGB", size=(width * col_num, height), color=0)
-for i in range(col_num):
-    result.paste(pattern, (i * pattern.width, 0))
+    result = Image.new(mode="RGB", size=(width * num_of_columns, height), color=0)
+    for i in range(num_of_columns):
+        result.paste(pattern, (i * pattern.width, 0))
 
-# ändra sen
-for i in range(num_of_slices):
-    pasteMask(i)
+    for i in range(num_of_slices):
+        pasteMask(i)
 
+    result.show()
 
-result.show()
+if __name__ == "__main__":
+    main()
